d8c23203b9a29ca09ad984cbe9cd36df61e8c5e1
Joshua Martin <joshuamartintx@gmail.com>
2025-08-05 19:38:46 -0500

    Update EncrypterBlackBox.py

--- EncrypterBlackBox.py
+++ EncrypterBlackBox.py
@@ -1,8 +1,17 @@
 from cryptography.hazmat.primitives.ciphers.aead import AESGCMSIV
 import sys
 import os
+import json
 from enum import Enum
-from source import FileEncrypter, FileDecrypter, KeyFragmentDistributor, KeyFragmenter
+from source import FileEncrypter, KeyFragmentDistributor, KeyFragmenter
+
+# temporary includes
+from cryptography.hazmat.primitives import serialization
+from secretshare import Secret, SecretShare, Share
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+# --- References --- #
+
 
 # --- Safety Principles --- #
 Debug_OutputGeneratedKey = False # This needs to be false for a secure run -> key should not be printed without this being true
@@ -40,17 +49,20 @@ def isValidKey(filePath: str, encryptionKey: keyType) -> bool:
     if (not fileExists):
         return False
 
-    # - open the file
-    with open(filePath) as file:
+    # - open the file and decrypt
+    encryptor = FileEncrypter.FileEncryptor(encryptionKey)
+    encryptor.decrypt_file(filePath, os.path.join(filePath, ".tmp"))
+
+    # - open the temp file, check if it is good
+    goodCanary = False
+    with os.path.join(filePath, ".tmp") as file:
         fileContents = file.read()
-        try:
-            f = Fernet(encryptionKey)
-            # - if successful; check if the contents match the known values
-            return f.decrypt(fileContents) == canaryFile_Content
-        except InvalidToken:
-            return False
-        
-    return True
+        goodCanary = (fileContents == canaryFile_Content)
+    
+    # - cleanup
+    os.remove(os.path.join(filePath, ".tmp"))
+
+    return goodCanary
 
 # --- Encryption routine functions --- #
 # AES-GCM used internally and for resource encryption. It is a symmetric key, so do not output it anywhere in a secure run
@@ -76,12 +88,12 @@ def encryptionRoutine(currPath, filepathsToEncrypt, keys_public, threshold: int)
     # --- Create metadata --- #
 
     # - output key fragments
-    KeyFragmentDistributor.output(shares_encrypted)
+    KeyFragmentDistributor.output(currPath, shares_encrypted)
 
     # - save decryption metadata
 
     # - create canary file
-    canaryPath = os.path.join(filePath, canaryFile_Name)
+    canaryPath = os.path.join(currPath, canaryFile_Name)
     createCanaryFile(currPath, Key_AESGCM)
 
 
@@ -92,9 +104,9 @@ def encryptionRoutine(currPath, filepathsToEncrypt, keys_public, threshold: int)
 
     # - encrypt canary file
     try:
-        if(encryptor.encryptFile(canaryPath, paths.join(canaryPath, ".tmp"))): # create encrypted version in temporary file
+        if(encryptor.encryptFile(canaryPath, os.paths.join(canaryPath, ".tmp"))): # create encrypted version in temporary file
             FileEncrypter.zero_out_file(canaryPath) # zero-out original file to eliminate non-encrypted data
-            os.replace(paths.join(canaryPath, ".tmp"), canaryPath) # then overwrite the original on success
+            os.replace(os.paths.join(canaryPath, ".tmp"), canaryPath) # then overwrite the original on success
     except:
         return False # DO NOT ALLOW ENCRYPTION IF WE FAIL TO ADD A CANARY
 
@@ -103,7 +115,7 @@ def encryptionRoutine(currPath, filepathsToEncrypt, keys_public, threshold: int)
     for f_in in filepathsToEncrypt:
         try:
             passed = False
-            passed = encryptor.encryptFile(f_in, paths.join(f_in, ".tmp")) # create encrypted version in temporary file
+            passed = encryptor.encryptFile(f_in, os.paths.join(f_in, ".tmp")) # create encrypted version in temporary file
         except:
             pass # recover
         if passed:
@@ -116,8 +128,8 @@ def encryptionRoutine(currPath, filepathsToEncrypt, keys_public, threshold: int)
     return True
 
 # --- Decryption routine functions --- #
-def decryptionRoutine() -> bool:
-    # - Retrieve provided key fragments
+def decryptionRoutine(currPath) -> bool:
+    # - Retrieve provided key fragments; look for log_encryption.txt in the current directory
 
     # - Combine key fragments into AES-GCM key
     genKey = KeyFragmenter.decryptAndAssembleFragments(keys_private, shares_encrypted, threshold, share_count_orig)
@@ -132,6 +144,52 @@ def decryptionRoutine() -> bool:
 
     # - All files decrypted, so return
     return True
+
+def serializationTest() :
+    message = "This is a long message that could support a 256 bit key"
+    print("input:", message)
+    my_key = message.encode('utf-8')
+    my_Secret = Secret(KeyFragmenter.encode_secret_from_bytes(my_key))
+
+    # - create public and private keys
+    public_keys = []
+    private_keys = []
+
+    for i in range(10):
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+        )
+        public_key = private_key.public_key().public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+        public_keys.append(public_key)
+        private_keys.append(private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.NoEncryption()))
+
+    # - Create json container
+    shares_json = {
+        "public_keys": public_keys,
+        "private_keys": private_keys,
+        "shares": [],
+        "share_positions": [],
+        "numShares": []
+    }
+    #print(shares_json)
+
+    shares_json_encrypted = KeyFragmenter.fragmentKeyAndEncrypt(my_Secret, shares_json, 4)
+    KeyFragmentDistributor.serializeJSON(os.getcwd(), shares_encrypted=shares_json_encrypted)
+    print("log written to:", os.getcwd())
+    exit()
+
+def deserializationTest():
+    recovered = KeyFragmentDistributor.deserializeJSON(os.getcwd())
+    my_key_recovered = KeyFragmenter.decryptAndAssembleFragments(recovered, 4, recovered["numShares"])
+    print("recovered:", KeyFragmenter.decode_secret_to_string(my_key_recovered.value))
+    exit()
 
 # --- Main --- #
 class RunMode(Enum):
